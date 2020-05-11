2de979ea3c070d394e06f6a9b95b32d799d464ec
Joakim Fors <joakim.fors@trivector.se>
2020-05-11 18:29:49 +0200

    Move route functions to utils and switch to multiple lookups using pgr_trsp

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from asyncpg import create_pool
 
-from api.utils import get_db
+from api.utils import get_db, pairwise, find_route
 from api.config import POSTGRES_DSN
 
 
@@ -60,6 +60,7 @@ class RouteQuery(BaseModel):
 
 class Segment(BaseModel):
     coords: List[Tuple[float, float]]
+    ts_klass: str
 
 
 class Route(BaseModel):
@@ -81,84 +82,21 @@ async def point(latlng: LatLng, db=Depends(get_db)):
 @app.post('/api/route')
 async def route(query: RouteQuery, db=Depends(get_db)):
     debug(query)
-
-    waypoints_sql = []
-    for i, waypoint in enumerate(query.waypoints):
-        waypoints_sql.append(f'({i}, ST_Transform(ST_SetSRID(ST_MakePoint('
-                             f'{waypoint.lng}, {waypoint.lat})'
-                             ', 4326), 3006))')
-    debug(waypoints_sql)
-
-    inner_sql = f'''
-        WITH waypoints(id, geom) AS (
-            VALUES {','.join(waypoints_sql)}
-        ), path(geom) AS (
-            SELECT ST_MakeLine(geom) FROM waypoints
-        ), weights(ts_klass, weight) AS (
-            VALUES
-                ('C1', 1), ('C2', 1.1), ('C3', 1.1),
-                ('B1', 1.2), ('B2', 1.3), ('B3', 1.5), ('B4', 1.9), ('B5', -1),
-                ('G1', 1.4), ('G2', 1.6)
-        )
-        SELECT
-            objectid as id,
-            from_vertex as source,
-            to_vertex as target,
-            shape_length * COALESCE(weight, -1) as cost
-        FROM cyklaiskane roads
-        JOIN path ON ST_DWithin(roads.geom, path.geom, 5000)
-        JOIN weights USING (ts_klass)
-    '''
-
-    route_sql = f'''
-        WITH waypoints(id, geom) AS (
-            VALUES {','.join(waypoints_sql)}
-        ), vias AS (
-            SELECT
-                point.id,
-                nearest.objectid,
-                nearest.fraction
-            FROM waypoints point,
-            LATERAL (
-                SELECT
-                  ST_LineLocatePoint(roads.geom, point.geom) fraction, roads.*
-                FROM (
-                  SELECT * FROM cyklaiskane ORDER BY geom <-> point.geom ASC LIMIT 10
-                ) roads
-                ORDER BY ST_Distance(roads.geom, point.geom) ASC
-                LIMIT 1
-            ) nearest
-        )
-        SELECT
-            roads.objectid,
-            roads.ts_klass,
-            roads.from_vertex,
-            roads.to_vertex,
-            roads.shape_length,
-            vias.id,
-            vias.fraction,
-            route.*,
-            ST_Transform(CASE WHEN route.id2 = roads.from_vertex THEN geom ELSE ST_Reverse(geom) END, 4326) as geom
-        FROM pgr_trspViaEdges('{inner_sql.replace("'", "''")}', (SELECT array_agg(objectid) FROM vias), (SELECT array_agg(fraction) FROM vias), FALSE, FALSE, 'SELECT * FROM cyklaiskane_restrictions') route
-        JOIN cyklaiskane roads ON route.id3 = roads.objectid
-        LEFT OUTER JOIN vias USING (objectid)
-        ORDER BY route.seq
-    '''
-    logging.debug(route_sql)
-
-    result = await db.fetch(route_sql)
-
     routes = []
-    route = None
-    for row in result:
-        if row['seq'] == 1:
-            route = Route()
-            routes.append(route)
-        #logging.debug(row['geom'].coords[:])
-        debug(row)
-        segment = Segment(coords=row['geom'].coords[:])
-        route.segments.append(segment)
+    route = Route()
+
+    for start, dest in pairwise(query.waypoints):
+        debug(start, dest)
+
+        result = await find_route(db, start, dest)
+
+        for row in result:
+            if row['waypoint_id'] is not None:
+                debug(row)
+            segment = Segment(coords=row['geom'].coords[:], ts_klass=row['ts_klass'])
+            route.segments.append(segment)
     #debug(routes)
+    routes.append(route)
     return routes
 
 
--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,8 @@
+import logging
 import shapely.geometry
 import shapely.wkb
+from devtools import debug
+from itertools import tee
 
 from fastapi import Request
 
@@ -23,3 +26,108 @@ async def get_db(request: Request):
             format='binary'
         )
         yield con
+
+
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+async def find_route(db, start, dest):
+    waypoints_sql = []
+    for i, waypoint in enumerate([start, dest]):
+        waypoints_sql.append(f'({i}, ST_Transform(ST_SetSRID(ST_MakePoint('
+                             f'{waypoint.lng}, {waypoint.lat})'
+                             ', 4326), 3006))')
+
+    inner_sql = f'''
+        WITH waypoints(id, geom) AS (
+            VALUES {','.join(waypoints_sql)}
+        ), path(geom) AS (
+            SELECT ST_MakeLine(geom) FROM waypoints
+        ), weights(ts_klass, weight) AS (
+            VALUES
+                ('C1', 1), ('C2', 1.1), ('C3', 1.1),
+                ('B1', 1.2), ('B2', 1.3), ('B3', 1.5), ('B4', 1.9), ('B5', -1),
+                ('G1', 1.4), ('G2', 1.6)
+        )
+        SELECT
+            objectid as id,
+            from_vertex as source,
+            to_vertex as target,
+            shape_length * COALESCE(weight, -1) as cost
+        FROM cyklaiskane roads
+        JOIN path ON ST_DWithin(roads.geom, path.geom, 5000)
+        JOIN weights USING (ts_klass)
+    '''
+
+    sql = f'''
+        WITH waypoints(id, geom) AS (
+            VALUES {','.join(waypoints_sql)}
+        ), vias AS (
+            SELECT
+                point.id waypoint_id,
+                nearest.objectid,
+                nearest.fraction
+            FROM waypoints point,
+            LATERAL (
+                SELECT
+                  ST_LineLocatePoint(roads.geom, point.geom) fraction, roads.*
+                FROM (
+                  SELECT *
+                  FROM cyklaiskane
+                  ORDER BY geom <-> point.geom ASC LIMIT 10
+                ) roads
+                ORDER BY ST_Distance(roads.geom, point.geom) ASC
+                LIMIT 1
+            ) nearest
+        )
+        SELECT
+            roads.objectid,
+            roads.ts_klass,
+            roads.from_vertex,
+            roads.to_vertex,
+            roads.shape_length,
+            vias.waypoint_id,
+            vias.fraction,
+            CASE WHEN vias.fraction IS NULL THEN 'full length' ELSE 'snip snip' END as bar,
+            route.*,
+            lead(route.id1) over () as route_to_vertex,
+            route.id1 != roads.from_vertex AND coalesce(lead(route.id1) over (), -1) != roads.to_vertex as reverse,
+            CASE WHEN route.id1 = roads.from_vertex OR coalesce(lead(route.id1) over (), -1) = roads.to_vertex THEN 'go ahead!' ELSE 'reverse!' END as foo,
+            ST_Transform(
+                CASE WHEN route.id1 = roads.from_vertex OR coalesce(lead(route.id1) over (), -1) = roads.to_vertex THEN
+                    CASE
+                    WHEN vias.fraction IS NULL THEN roads.geom
+                    WHEN route.id1 = -1 THEN ST_LineSubstring(roads.geom, vias.fraction, 1)
+                    ELSE ST_LineSubstring(roads.geom, 0, vias.fraction)
+                    END
+                ELSE
+                    ST_Reverse(
+                        CASE
+                        WHEN vias.fraction IS NULL THEN roads.geom
+                        WHEN route.id1 = -1 THEN ST_LineSubstring(roads.geom, 0, vias.fraction)
+                        ELSE ST_LineSubstring(roads.geom, vias.fraction, 1)
+                        END
+                    )
+                END,
+                4326
+            ) as geom
+        FROM pgr_trsp(
+            '{inner_sql.replace("'", "''")}',
+            (SELECT array_agg(objectid) FROM vias)[1],
+            (SELECT array_agg(fraction) FROM vias)[1],
+            (SELECT array_agg(objectid) FROM vias)[2],
+            (SELECT array_agg(fraction) FROM vias)[2],
+            FALSE,
+            FALSE,
+            'SELECT * FROM cyklaiskane_restrictions'
+        ) route
+        JOIN cyklaiskane roads ON route.id2 = roads.objectid
+        LEFT OUTER JOIN vias USING (objectid)
+        ORDER BY route.seq
+    '''
+    logging.debug(sql)
+
+    return await db.fetch(sql)
