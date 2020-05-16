dde320953cce6a06f1f77986b2a0df99dd280b7e
Joakim Fors <joakim.fors@trivector.se>
2020-05-16 03:39:16 +0200

    Use Starlettes Config class to read configuration

--- api/config.py
+++ api/config.py
@@ -1,4 +1,11 @@
-from os import getenv
+from databases import DatabaseURL
+from starlette.config import Config
+from starlette.datastructures import Secret
 
-POSTGRES_DSN = getenv('POSTGRES_DSN',
-                      'postgres://velociped:foobar@localhost:5433/velociped')
+config = Config('.env')
+
+POSTGRES_DSN = config('POSTGRES_DSN', cast=DatabaseURL)
+
+LM_CLIENT_ID = config('LM_CLIENT_ID', cast=Secret)
+LM_CLIENT_SECRET = config('LM_CLIENT_SECRET', cast=Secret)
+LM_TOKEN_URL = config('LM_TOKEN_URL')
