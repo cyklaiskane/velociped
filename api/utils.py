a4650198c9104bf5d8d60bde4b6ef282dd12f4f7
Joakim Fors <joakim.fors@trivector.se>
2020-05-19 15:57:10 +0200

    Use CTE to group sequential route parts of same type

--- api/utils.py
+++ api/utils.py
@@ -90,64 +90,88 @@ async def find_route(start: LatLng, dest: LatLng, profile: int = 1) -> Iterable:
                 ORDER BY ST_Distance(roads.geom, point.geom) ASC
                 LIMIT 1
             ) nearest
+        ), parts AS (
+            SELECT
+                roads.objectid,
+                roads.ts_klass,
+                roads.from_vertex,
+                roads.to_vertex,
+                roads.shape_length,
+                roads.namn_130 as name,
+                route.cost / weights.weight as length,
+                route.cost / weights.penalty as duration,
+                vias.waypoint_id,
+                vias.fraction,
+                CASE WHEN vias.fraction IS NULL
+                    THEN 'full length'
+                    ELSE 'snip snip'
+                END as bar,
+                route.*,
+                lead(route.id1) over () as route_to_vertex,
+                (
+                    lag(ts_klass) OVER () IS DISTINCT FROM ts_klass
+                    OR lag(namn_130) OVER () IS DISTINCT FROM namn_130
+                )::int as part_start,
+                ST_Transform(
+                    CASE
+                        WHEN route.id1 = roads.from_vertex
+                             OR lead(route.id1, 1, -1) OVER () = roads.to_vertex
+                        THEN
+                            CASE
+                                WHEN vias.fraction IS NULL
+                                THEN roads.geom
+                                WHEN route.id1 = -1
+                                THEN ST_LineSubstring(roads.geom, vias.fraction, 1)
+                                ELSE ST_LineSubstring(roads.geom, 0, vias.fraction)
+                            END
+                    ELSE
+                        ST_Reverse(
+                            CASE
+                                WHEN vias.fraction IS NULL
+                                THEN roads.geom
+                                WHEN route.id1 = -1
+                                THEN ST_LineSubstring(roads.geom, 0, vias.fraction)
+                                ELSE ST_LineSubstring(roads.geom, vias.fraction, 1)
+                            END
+                        )
+                    END,
+                    4326
+                ) as geom
+            FROM pgr_trsp(
+                '{inner_sql.replace("'", "''")}',
+                (SELECT array_agg(objectid) FROM vias)[1],
+                (SELECT array_agg(fraction) FROM vias)[1],
+                (SELECT array_agg(objectid) FROM vias)[2],
+                (SELECT array_agg(fraction) FROM vias)[2],
+                TRUE,
+                TRUE,
+                'SELECT * FROM cyklaiskane_restrictions'
+            ) route
+            JOIN cyklaiskane roads ON route.id2 = roads.objectid
+            JOIN weights USING (ts_klass)
+            LEFT OUTER JOIN vias USING (objectid)
+            ORDER BY route.seq
         )
         SELECT
-            roads.objectid,
-            roads.ts_klass,
-            roads.from_vertex,
-            roads.to_vertex,
-            roads.shape_length,
-            roads.namn_130 as name,
-            route.cost / weights.weight as length,
-            route.cost / weights.penalty as duration,
-            vias.waypoint_id,
-            vias.fraction,
-            CASE WHEN vias.fraction IS NULL
-                THEN 'full length'
-                ELSE 'snip snip'
-            END as bar,
-            route.*,
-            lead(route.id1) over () as route_to_vertex,
-            ST_Transform(
-                CASE
-                    WHEN route.id1 = roads.from_vertex
-                         OR coalesce(lead(route.id1) OVER (), -1) = roads.to_vertex
-                    THEN
-                        CASE
-                            WHEN vias.fraction IS NULL
-                            THEN roads.geom
-                            WHEN route.id1 = -1
-                            THEN ST_LineSubstring(roads.geom, vias.fraction, 1)
-                            ELSE ST_LineSubstring(roads.geom, 0, vias.fraction)
-                        END
-                ELSE
-                    ST_Reverse(
-                        CASE
-                            WHEN vias.fraction IS NULL
-                            THEN roads.geom
-                            WHEN route.id1 = -1
-                            THEN ST_LineSubstring(roads.geom, 0, vias.fraction)
-                            ELSE ST_LineSubstring(roads.geom, vias.fraction, 1)
-                        END
-                    )
-                END,
-                4326
-            ) as geom
-        FROM pgr_trsp(
-            '{inner_sql.replace("'", "''")}',
-            (SELECT array_agg(objectid) FROM vias)[1],
-            (SELECT array_agg(fraction) FROM vias)[1],
-            (SELECT array_agg(objectid) FROM vias)[2],
-            (SELECT array_agg(fraction) FROM vias)[2],
-            TRUE,
-            TRUE,
-            'SELECT * FROM cyklaiskane_restrictions'
-        ) route
-        JOIN cyklaiskane roads ON route.id2 = roads.objectid
-        JOIN weights USING (ts_klass)
-        LEFT OUTER JOIN vias USING (objectid)
-        ORDER BY route.seq
+            array_agg(objectid) as ids,
+            array_agg(seq ORDER BY seq) as seq,
+            array_agg(part_start ORDER BY seq) as starts,
+            array_agg(part_valid) as part_valids,
+            array_agg(ST_AsText(geom)) as part_wkt,
+            part,
+            ts_klass,
+            name,
+            SUM(length) as length,
+            SUM(duration) as duration,
+            MIN(waypoint_id) as waypoint_id,
+            ST_IsValidDetail(ST_MakeLine(geom ORDER BY seq)) as valid_geom,
+            ST_AsText(ST_MakeLine(geom ORDER BY seq)) as geom_wkt,
+            ST_MakeLine(geom ORDER BY seq) as geom
+        FROM (SELECT *, SUM(part_start) OVER (ORDER BY seq) part, ST_IsValid(geom) part_valid FROM parts) _
+        WHERE GeometryType(geom) = 'LINESTRING'
+        GROUP BY part, ts_klass, name
+        ORDER BY part
     '''
-    logging.debug(sql)
+    #logging.debug(sql)
 
     return await db.fetch_all(sql)
