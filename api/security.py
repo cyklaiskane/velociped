ffac9f5683be7ee561a2104cad610bc1b5f4b6fe
Joakim Fors <joakim.fors@trivector.se>
2020-08-27 16:39:41 +0200

    Set token name when updating

--- api/security.py
+++ api/security.py
@@ -33,7 +33,7 @@ async def update_token(
     debug(name, token, refresh_token, access_token)
     for i, _token in enumerate(tokens):
         if _token.name == name and _token.access_token == access_token:
-            tokens[i] = Token(**token)
+            tokens[i] = Token(name=name, **token)
             return
     tokens.append(Token(name=name, **token))
     debug(tokens)
