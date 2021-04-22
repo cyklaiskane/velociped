6e7be580e1787b04795751b2a860040e72c1e6d3
Joakim Fors <joakim.fors@trivector.se>
2021-04-22 14:16:58 +0200

    build: output bundle to assets dir

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -7,5 +8,8 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './dist',
     open: false,
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
   },
 });
